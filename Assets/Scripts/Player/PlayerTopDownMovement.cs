82ba437f153b9bc61edb22f41c9220565789cee9
Claive <abbosjabborov12@gmail.com>
2025-09-05 15:19:19 +0500

    Update: Player movement and camera follow added

--- Assets/Scripts/Player/PlayerTopDownMovement.cs
+++ Assets/Scripts/Player/PlayerTopDownMovement.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class PlayerTopDownMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Animator animator;
+    private Vector2 movement;
+    
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Input
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        // Animation
+        if (movement != Vector2.zero)
+        {
+            animator.SetFloat("Horizontal", movement.x);
+            animator.SetFloat("Vertical", movement.y);
+            animator.SetFloat("Speed", movement.sqrMagnitude);
+        }
+    }
+    void FixedUpdate()
+    {
+        // Movement
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 }
